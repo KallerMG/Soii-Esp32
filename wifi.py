167d8eccf837661c21c843b34b1040eece34a670
KallerMG <50585470+KallerMG@users.noreply.github.com>
2019-05-28 15:15:30 -0300

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,12 +1,7 @@
 import network
-WiFi_SSID = "nome do wifi "
-WiFi_PASS = " sua senha"
-def do_connect():
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(WiFi_SSID, WiFi_SSID)
-        while not wlan.isconnected():
-            pass
-    print('network config:')
+def conectar():
+  station = network.WLAN(network.STA_IF)
+  station.active(True)
+  if not station.isconnected():
+    station.connect("seu wifi", "sua senha")
+    return (station.ifconfig())
