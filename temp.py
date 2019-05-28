1fe1981449a63f395db9b2be049940a3ec0e4043
KallerMG <50585470+KallerMG@users.noreply.github.com>
2019-05-28 15:10:18 -0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
-import machine
-adc = machine.ADC(machine.Pin(36))
-print(adc.read())
-   
+def temperatura(): #LM35 valor aproximado 
+  import machine
+  adc = machine.ADC(machine.Pin(36))
+  temp = adc.read()
+  return (temp*(3.3/1024)*10)
