c0d4c639047a0f2b7f6bb4c555aba898de2db74b
KallerMG <50585470+KallerMG@users.noreply.github.com>
2019-05-28 15:08:21 -0300

    Update ThingSpeakFinal.py

--- ThingSpeakFinal.py
+++ ThingSpeakFinal.py
@@ -5,8 +5,8 @@ from umqtt.simple import MQTTClient
 print (conectar())
 servidor = "mqtt.thingspeak.com"
 client = MQTTClient("umqtt_client", servidor)
-idCanal = "789117"
-ApiKey = "6WS8SD0T6DVHVA7D"
+idCanal = "id do canal"
+ApiKey = "api key do ThingSpeak"
 topico = "channels/" + idCanal + "/publish/" + ApiKey
 a = 1
 while a == 1:
